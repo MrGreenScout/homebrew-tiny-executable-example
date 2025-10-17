65884c2b8d6a698f693365eb40d8f43a4c3142ac
MrGreenScout <116956970+MrGreenScout@users.noreply.github.com>
2025-10-17 22:00:07 +0200

    Added Formula

---
+++ Formula/tiny-executable-example.rb
@@ -0,0 +1,38 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class TinyExecutableExample < Formula
+  desc ""
+  homepage "https://github.com/MrGreenScout/homebrew-tiny-executable-example"
+  url "https://github.com/MrGreenScout/homebrew-tiny-executable-example/archive/refs/tags/v0.0.1-alpha.tar.gz"
+  sha256 "33fe363cb46e09194a85bc87e4398e32a21cd4a02a62f9c53b34077e1376b914"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tiny-executable-example`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "#{bin}/tiny-executable-example", "mgsfilename.mgs", "-o", "outputfile", "-savc"
+  end
+end
